89873176604fd122d5289cc4bf4b9023c4a55bb8
Crafton Williams <740320+crafton@users.noreply.github.com>
2021-08-05 08:41:06 +1000

    Separate the get and update functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
 import * as functions from "firebase-functions";
-import {ProxyResponse} from "./models/ProxyResponse";
 
-export const postmanDtProxy = functions.region("australia-southeast1")
+export const getDtContacts = functions.region("australia-southeast1")
     .https.onCall((data, context) => {
       if (context.app == undefined) {
         throw new functions.https.HttpsError(
@@ -9,11 +8,25 @@ export const postmanDtProxy = functions.region("australia-southeast1")
             "The function must be called from a verified app."
         );
       }
-      const proxyResponse: ProxyResponse = {
-        message: "Received a valid message",
+      return {
+        message: "Received a valid message to retrieve contacts",
         statusCode: 200,
       };
 
       //    functions.logger.info("Hello logs!", {structuredData: true});
-      return proxyResponse;
+    });
+
+export const updateDtPostageStatus = functions.region("australia-southeast1")
+    .https.onCall((data, context) => {
+      if (context.app == undefined) {
+        throw new functions.https.HttpsError(
+            "failed-precondition",
+            "The function must be called from a verified app."
+        );
+      }
+
+      return {
+        message: "Received a valid message to update postage state.",
+        statusCode: 200,
+      };
     });
