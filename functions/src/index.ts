1cebea3d80b7c82dc7f94b0d265da2d41e3a712d
Crafton Williams <740320+crafton@users.noreply.github.com>
2021-08-04 21:33:59 +1000

    Modify call method to use onCall so appcheck is supported

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,24 +2,18 @@ import * as functions from "firebase-functions";
 import {ProxyResponse} from "./models/ProxyResponse";
 
 export const postmanDtProxy = functions.region("australia-southeast1")
-    .https.onRequest((request, response) => {
+    .https.onCall((data, context) => {
+      if (context.app == undefined) {
+        throw new functions.https.HttpsError(
+            "failed-precondition",
+            "The function must be called from a verified app."
+        );
+      }
       const proxyResponse: ProxyResponse = {
-        message: "Unrecognized",
-        statusCode: 400,
+        message: "Received a valid message",
+        statusCode: 200,
       };
 
-      switch (request.method) {
-        case "GET":
-          proxyResponse.message = "Hey, I just received a GET!";
-          proxyResponse.statusCode = 200;
-          break;
-        case "PUT":
-          proxyResponse.message = "Hey, I just got PUT-ed";
-          proxyResponse.statusCode = 200;
-          break;
-        default:
-          break;
-      }
       //    functions.logger.info("Hello logs!", {structuredData: true});
-      response.status(proxyResponse.statusCode).json(proxyResponse);
+      return proxyResponse;
     });
